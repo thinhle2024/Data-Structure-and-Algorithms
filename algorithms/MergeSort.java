7c2cad462534a488348c412046bc08a9a9de9762
thinhle2024 <thinhquocle92@gmail.com>
2024-11-22 21:32:45 -0600

    refactored code

--- algorithms/MergeSort.java
+++ algorithms/MergeSort.java
@@ -4,29 +4,27 @@ import java.util.Arrays;
 
 public class MergeSort {
     private static void merge(int[] arr, int[] left, int[] right) {
-        int l = 0, r = 0; // used for array traversal (left[l++] and right[r++])
-        int p = 0; // smaller value between left[l] and right[r] is merged into array arr at pivot p (or arr[p])
+        int l = 0, r = 0; // left[l++] and right[r++]
+        int a = 0; // arr[a++]
 
         // merge array left and array right into array arr
-
         while (l < left.length && r < right.length) { // bound checking
+            // merges the smaller value between left[l] and right[r] into arr
             if (left[l] <= right[r]) {
-                arr[p++] = left[l++];
+                arr[a++] = left[l++];
             } else {
-                arr[p++] = right[r++];
+                arr[a++] = right[r++];
             }
         }
 
         // merge the remaining of array left if any
-
         while (l < left.length) { // bound checking
-            arr[p++] = left[l++];
+            arr[a++] = left[l++];
         }
 
         // merge the remaining of array right if any
-
         while (r < right.length) { // bound checking
-            arr[p++] = right[r++];
+            arr[a++] = right[r++];
         }
     }
 
@@ -36,13 +34,17 @@ public class MergeSort {
 
         int mid = arr.length / 2;
 
-        int[] left = Arrays.copyOfRange(arr, 0, mid); // 0 -> mid - 1 (inclusive)
-        int[] right = Arrays.copyOfRange(arr, mid, arr.length); // mid -> len - 1 (inclusive)
+        int[] left = Arrays.copyOfRange(arr, 0, mid); // [0, mid - 1]
+        int[] right = Arrays.copyOfRange(arr, mid, arr.length); // [mid, len - 1]
 
+        // gets to the leaf nodes of the tree before
         divide(left);
         divide(right);
 
+        // performs sorting
         merge(arr, left, right);
+
+        // => bottom-up approach
     }
 
     public static void sort(int[] arr) {
--- algorithms/QuickSort.java
+++ algorithms/QuickSort.java
@@ -1,10 +1,8 @@
 package algorithms;
 /*
- * Values to the left of a pivot is less than or equal to the pivot. 
- * Values to the right of the pivot is more than or equal to the pivot. 
- * Keep partition the array in that manner until all values are sorted. 
- * That is the gist of quick sort. Merge sort uses bottom-up approach, 
- * whereas quick sort uses top-down approach.
+ * Partitioning an array: Values to the left of a pivot is less than 
+ * or equal to the pivot. Values to the right of the pivot is greater
+ * than or equal to the pivot. 
  */
 
 public class QuickSort {
@@ -16,8 +14,8 @@ public class QuickSort {
     }
 
     /***
-     * Partitions an array into two subarrays. Values of the left
-     * subarray <= a pivot. Values of the right subarray >= the pivot.
+     * Partitions an array into two subarrays. All values of one of the
+     * subarray <= a pivot. all values of the other subarray >= the pivot.
      * 
      * @param arr  unordered array
      * @param low  index of the first element of a subarray
@@ -25,19 +23,17 @@ public class QuickSort {
      * @return index of the pivot
      */
     private static int partition(int[] arr, int low, int high) {
-        int indexPivot = low - 1; // presume pivot <= all values
+        int indexPivot = low - 1; // criteria: values <= pivot <= other values
 
-        // rearranges values in a way that maintains the forementioned criteria.
-        while (low < high) { // "< high" ~ not having to use an extra
-                             // variable (in-place algorithm principle)
-            if (arr[low] <= arr[high]) {
-                indexPivot++;
-                swap(arr, indexPivot, low);
+        // rearranges values in a way that maintains the aforementioned criteria.
+        while (low < high) { 
+            if (arr[low] <= arr[high]) { // arr[high] is the pivot
+                swap(arr, ++indexPivot, low);
             }
-
             low++;
         }
 
+        // moves the pivot to its supposed place (ensuring the above criteria)
         swap(arr, ++indexPivot, high);
 
         return indexPivot;
@@ -45,13 +41,14 @@ public class QuickSort {
 
     public static void sort(int[] arr, int low, int high) {
         if (low < high) {
-
             // sorts this layer
             int indexPivot = partition(arr, low, high);
 
-            // sorts sub layers
+            // sorts sublayers
             sort(arr, low, indexPivot - 1);
             sort(arr, indexPivot + 1, high);
+
+            // performs sorting at each layer => top-down approach
         }
     }
 
